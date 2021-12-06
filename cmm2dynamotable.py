a01cc2896b7ea2dec54518cf0b4a69a4a12bb74a
builab <huy.bui@mcgill.ca>
2021-12-06 14:14:19 -0500

    Add files via upload

---
+++ cmm2dynamotable.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 13:33:47 2021
+
+Trim the Dynamo table based on a Chimera cmm file
+Using dynamo_table2cmm to generate the cmm file from 1 tomogram
+Load the cmm & tomo in Chimera
+Delete unnecessary particles on carbon etc
+Save the cmm as a new file
+Generate new table
+
+python cmm2dynamotable.py --i table.tbl --o table_out.tbl --cmm sel_markers.cmm
+
+@author: kbui2
+"""
+
+import numpy as np
+import argparse
+import xml.etree.ElementTree as ET
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(description='Write new table based on selected Cmm file')
+	parser.add_argument('--i', help='Input table file',required=True)
+	parser.add_argument('--o', help='Output table file',required=True)
+	parser.add_argument('--cmm', help='Chimera cmm file',required=True)
+
+	args = parser.parse_args()
+
+	intable = np.genfromtxt(args.i, delimiter=' ')
+	markerset = ET.parse(args.cmm)
+	root = markerset.getroot()
+	
+	listpar = []
+	# Obtain all marker id
+	for marker in root.iter('marker'):
+		listpar.append(int(marker.attrib['id']) - 1)
+		
+	#listnp = np.array(listpar)
+	outtable = intable[listpar, :]
+	print('Writing ' + args.o)
+	np.savetxt(args.o, outtable, fmt='%.2f', delimiter=' ')
+	
