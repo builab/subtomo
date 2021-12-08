d0c89e00afa42d85510dabe64cc6c4665a6adb07
builab <huy.bui@mcgill.ca>
2021-12-08 11:46:56 -0500

    Update batch_mkdir_tiltseries.py

--- batch_mkdir_tiltseries.py
+++ batch_mkdir_tiltseries.py
@@ -3,10 +3,30 @@
 """
 Created on Sat Dec  8, 2021
 
+All .mrc & mrc.mdoc file should be in 1 folder
+Move .mrc file into folders
+
 @author: Huy Bui, McGill
 """
 import argparse, os, glob
 
 if __name__=='__main__':
- parser = argparse.ArgumentParser(description='Batch ctffind4 of many tilt series')
-	parser.add_argument('--i', help='Input Tilt Series',required=True)
+	parser = argparse.ArgumentParser(description='Organize tilt series into folders')
+	parser.add_argument('--i', help='Input Tilt Series Wild Card',required=True)
+	
+	tsList = glob.glob(args.i)
+	for tiltseries in tsList:
+		if not tsName.endswith('.mrc'):
+			print('Processing ' + tiltseries + ': ERROR! Only mrc file supported')
+			break
+		tsName = tiltseries.replace('.mrc', '')
+		mrcDoc = tiltseries.replace('.mrc', '.mrc.mdoc')
+		#os.mkdir(tsName)
+		# Move tilt series
+		print('mv ' + tiltseries + ' ' + os.path.join(tsName, tiltseries))
+		print('mv ' + mrcDoc + ' ' + os.path.join(tsName, mrcDoc))
+		#shutil.move(tiltseries, os.path.join(tsName, tiltseries))
+		#shutil.move(mrcDoc, os.path.join(tsName, mrcDoc))
+
+
+
