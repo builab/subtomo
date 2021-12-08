f61756bc1e2c4037caba25fe186a07d63fc5c05b
builab <huy.bui@mcgill.ca>
2021-12-08 12:05:46 -0500

    Update batch_tomo_reconstruct.py

--- batch_tomo_reconstruct.py
+++ batch_tomo_reconstruct.py
@@ -5,10 +5,20 @@ Created on Dec 8 2021
 Script to run batch imod through the command line
 Only use for newst & tilt & tilt_sirt for now
 
+This is used specifically for K3 in McGill.
+
 @author: Huy Bui, McGill
 """
+
+import argparse, os
+
 if __name__=='__main__':
-  parser = argparse.ArgumentParser(description='Batch ctffind4 of many tilt series')
+	parser = argparse.ArgumentParser(description='Batch tomo reconstruct using IMOD')
 	parser.add_argument('--i', help='Input Tilt Series',required=True)
-	parser.add_argument('--angpix', help='Pixel size of the tilt series',required=True)
-	parser.add_argument('--ctffind_exe', help='Path to Ctffind executable',required=True)
+	parser.add_argument('--operation', help='Imod operation (newst or tilt or tilt_sirt)',required=True)
+	parser.add_argument('--bin', help='Bin factor',required=True)
+	
+	
+	print('Tested with IMOD 4.9 only')
+	
+	args = parser.parse()
