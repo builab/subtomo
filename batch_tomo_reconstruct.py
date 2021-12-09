fc981f47d6322e19c07beb367257b5276a87bc8f
builab <huy.bui@mcgill.ca>
2021-12-08 21:37:37 -0500

    Update batch_tomo_reconstruct.py

--- batch_tomo_reconstruct.py
+++ batch_tomo_reconstruct.py
@@ -4,7 +4,7 @@
 Created on Dec 8 2021
 Script to run batch imod through the command line
 Only use for newst & tilt & tilt_sirt for now
-
+For now, use a default template but should be flexible in the future for generating different bin tomo
 
 This is used specifically for K3 in McGill. Imod 4.11.8
 
@@ -13,10 +13,42 @@ This is used specifically for K3 in McGill. Imod 4.11.8
 
 import argparse, os, glob
 
-def run_com(operation, tsName, tempCont)
+def run_newst(operation, tsName, tempCont):
 	'''Write newst and run'''
-	outCom = open('newst.com', 'w')
-	while outCom:
+	outCom = open(operation + '.com', 'w')
+	baseName = tsName.replace('.mrc','')
+	for line in tempCont:
+		if line.startswith('InputFile'):
+			outCom.write('InputFile\t{:s}.mrc\n'.format(baseName)
+		elif line.startswith('OutputFile'):
+			outCom.write('OutputFile\t{:s}_ali.mrc\n'.format(baseName)
+		elif line.startswith('TransformFile'):
+			outCom.write('TransformFile\t{:s}.xf\n'.format(baseName)
+		#elif line.startswith('BinByFactor'):
+		#	outCom.write('BinByFactor\t{:d}.xf\n'.format(binFactor)
+		else:
+			outCom.write(line)
+	outCom.close()
+				     
+				     
+def run_tilt(operation, tsName, tempCont):
+	outCom = open('tilt.com', 'w')	
+	baseName = tsName.replace('.mrc','')
+				     
+	for line in tempCont:
+		if line.startswith('InputProjections'):
+			outCom.write('InputProjections\t{:s}_ali.mrc\n'.format(baseName)
+		elif line.startswith('OutputFile'):
+			outCom.write('OutputFile\t{:s}_full_rec.mrc\n'.format(baseName)
+		elif line.startswith('TILTFILE'):
+			outCom.write('TILTFILE\t{:s}.tlt\n'.format(baseName)
+		elif line.startswith('THICKNESS'):
+			outCom.write('THICKNESS\t{:s}\n'.format(thickness)
+		elif line.startswith('XTILTFILE'):
+			outCom.write('XTILFILE\t{:s}.xtilt\n'.format(baseName)
+		else:
+			outCom.write(line)
+	outCom.close()		
 		
 	
 	
@@ -25,14 +57,16 @@ if __name__=='__main__':
 	parser = argparse.ArgumentParser(description='Batch tomo reconstruct using IMOD')
 	parser.add_argument('--i', help='Input Tilt Series',required=True)
 	parser.add_argument('--operation', help='Imod operation (newst or tilt or tilt_sirt)',required=True)
-	parser.add_argument('--bin', help='Bin factor',required=True)
+	parser.add_argument('--bin', help='Bin factor',required=True)			     
 	parser.add_argument('--template', help='Template file from 1 reconstruction',required=True)
+	#parser.add_argument('--xsize', help='X size of image',required=True)	
+	#parser.add_argument('--ysize', help='Y size of image',required=True)			     
 	
 	print('Only tested with IMOD 4.9 and 4.11.8 only')
 	
 	args = parser.parse()
 	     
-	binFactor = float(args.bin)
+	binFactor = int(args.bin)
 	
 	tempfile = open(args.template, 'r')
 	tempCont = tempfile.readlines()
