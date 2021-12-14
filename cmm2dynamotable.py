95f34eb2190539e75693580635c7572cd35fe29a
builab <huy.bui@mcgill.ca>
2021-12-13 21:42:22 -0500

    Update cmm2dynamotable.py

--- cmm2dynamotable.py
+++ cmm2dynamotable.py
@@ -40,7 +40,7 @@ if __name__=='__main__':
 	#listnp = np.array(listpar)
 	outtable=intable[np.isin(intable[:, 0], listpar)]
 	#print(intable[:, 0])
-	print(listpar)
+	#print(listpar)
 	print('Writing ' + args.o)
 	np.savetxt(args.o, outtable, fmt='%.2f', delimiter=' ')
 	
